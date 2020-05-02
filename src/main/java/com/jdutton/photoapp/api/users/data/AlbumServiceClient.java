47b7d9eea1256bfab2f2777aca5b1638323ef8f7
Jorge Dutton <jorge.dutton@gmail.com>
2020-05-02 07:59:48 +0200

    Added support for Hystrix circuit breaker

--- src/main/java/com/jdutton/photoapp/api/users/data/AlbumServiceClient.java
+++ src/main/java/com/jdutton/photoapp/api/users/data/AlbumServiceClient.java
@@ -1,17 +1,29 @@
 package com.jdutton.photoapp.api.users.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.jdutton.photoapp.api.users.ui.model.AlbumResponseModel;
 
-@FeignClient(name = "albums-ws")
+@FeignClient(name = "albums-ws", fallback = AlbumsFallback.class)
 public interface AlbumServiceClient {
 
-	@GetMapping("/users/{id}/albumss")
+	@GetMapping("/users/{id}/albums")
 	public List<AlbumResponseModel> getAlbums(@PathVariable final String id);
+
+}
+
+@Component
+class AlbumsFallback implements AlbumServiceClient {
+
+	@Override
+	public List<AlbumResponseModel> getAlbums(String id) {
+		return new ArrayList<>();
+	}
 
 }
